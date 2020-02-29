c86df7267e01dd44f28dbdf798b81ebc74eb444e
burakkilicaslan <b.kilicaslanbk@gmail.com>
2020-02-29 09:36:30 +0300

    Adjustments

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/burakkilicaslan/Desktop/Yazılım-Proje/Django-rest/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
